5d66555a4d0242e8504982b81e128130e8bf86d9
SouksavathPMS <kaitk452@gmail.com>
2023-11-17 21:06:51 +0700

    Add bottom navigation bar

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# super_shy_banking
 
-A new Flutter project.
+# SUPER SHY BANKING
+ໂປຣເຈັກນີ້ແມ່ນສ້າງຂື້ນມາເພື່ອນຳໃຊ້ເຂົ້າໃນການແຂ່ງຂັນງານ Comet Hackathon 2023 ພາຍໃຕ້ຫົວຂໍ້ທີ່ວ່າ: " ການນຳເອົາ Software ທີ່ມີຢູ່ໃນລາວ ແລະ ນຳມາປັບປຸງໃຫ້ມີປະສິດທິພາບຫຼາຍຂື້ນ ໂດຍນຳໃຊ້ flutter ເພື່ອພັດທະນາໃຫ້ຢູ່ໃນລັກສະນະ module ຫຼື package "
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
+## Core Features
 
-A few resources to get you started if this is your first Flutter project:
+- Basic Data Visualization (ການເບິ່ງພາຍລວມລາຍຮັບ-ຈ່າຍ)
+- Payment Category (ການເບິ່ງປະເພດຂອງການຈ່າຍ)
+- Customizable Payment Filters (ການເບິ່ງປະຫວັດການໃຊ້-ຈ່າຍຕົວການມີຕົວເລືອກ)
+- Improved UX/UI Experience (ປັບປຸງການອອກແບບ && ປະສົບການຜູ້ໃຊ້)
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Comet Hackathon 2023 
+
+[Comet Digital Agency ](https://www.facebook.com/CometDigitalAgency)
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:super_shy_banking/screens/bussiness_screen.dart';
+import 'package:super_shy_banking/screens/home_screen.dart';
+import 'package:super_shy_banking/screens/school_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,22 +23,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    BussinessScreen(),
+    SchoolScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AppBarText'),
+        title: const Text('SUPERSHYBANKING'),
       ),
-      body: const Center(
-        child: Text('Body'),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        // selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
